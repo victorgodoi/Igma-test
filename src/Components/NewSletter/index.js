29411ce29fb26b08694f3c1ac95e7c87b1efff53
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2024-02-01 16:00:15 -0300

    add invalid no imput e add mais variable

--- src/Components/NewSletter/index.js
+++ src/Components/NewSletter/index.js
@@ -6,13 +6,133 @@ const newSletter = () => {
   return (
     <div id={style.divNewSletter}>
       <div>
-        <p></p>
-        <p></p>
+        <p>Assine nossa newsletter</p>
+        <p>
+          Desenvolva-se conosco com conteúdos sobre Tecnologia e Design a cada
+          15 dias.
+        </p>
         <Input nome="Nome" />
         <Input nome="Email" />
         <Button />
       </div>
-      <div></div>
+      <div>
+        <ul>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+          <li>
+            <img
+              src=""
+              width=""
+              height=""
+              alt="imagem celular"
+              title="imagem celular"
+            />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
